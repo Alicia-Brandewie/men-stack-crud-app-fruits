368c4783611f0080b6c6344b155c8a927c09a69f
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-18 12:16:11 -0500

    DB added

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const dotenv = require('dotenv'); // bringing the functionality of dotenv
+dotenv.config(); // using dotenv to bring the variables from the .env file
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI);
 
+mongoose.connection.on ('connected',()  => {
+    console.log(`connected to MongoDB ${mongoose.connection.name}`);
+}) //when DB is connected, similar to the listen method 
 
 // server.js
 
@@ -10,27 +18,6 @@ const app = express();
 app.get("/", async (req, res) => {
   res.render('index.ejs');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
